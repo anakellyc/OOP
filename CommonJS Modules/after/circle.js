2e67a7fd6933b2ff5289b6ff61dff4a70ef7841d
anakellyc <anakcampos@gmail.com>
2019-05-01 00:03:13 +0200

    all exercises completed

--- CommonJS Modules/after/circle.js
+++ CommonJS Modules/after/circle.js
@@ -1,15 +1,18 @@
-// Implementation Detail 
+// Implementation Detail - not exported
 const _radius = new WeakMap();
 
-// Public Interface
+// Public Interface - module exported
 class Circle {
   constructor(radius) {
     _radius.set(this, radius);
   }
 
   draw() {
-    console.log('Circle with radius ' + _radius.get(this));
+    console.log("Circle with radius " + _radius.get(this));
   }
 }
 
 module.exports = Circle;
+// If you have more than one moddule:
+//module.exports.Circle = Circle;
+//module.exports.ANOTHERMODULE = AnotherModule
